59ee7f2edfa9e2dc3d1423de0288d051e91e4946
Marcin Wojciechowski <marcin@valointranet.com>
2020-11-24 16:19:47 +0100

    Group type returned

--- MGWDev.GraphSDK.UT/MGWDevGraphSDK.UT.Tests/Graph/MeEndpointTests.cs
+++ MGWDev.GraphSDK.UT/MGWDevGraphSDK.UT.Tests/Graph/MeEndpointTests.cs
@@ -1,6 +1,7 @@
 ﻿using MGWDevGraphSDK.UT.Tests.Mock;
 using Microsoft.Graph;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -49,6 +50,7 @@ namespace MGWDevGraphSDK.UT.Tests.Graph
         [TestMethod]
         public void MeEndpoint_GetMeInfo_MemberOf()
         {
+            var sth = JsonConvert.DeserializeObject("{\"@odata.type\": \"#microsoft.graph.group\"}");
             string requestUrl = "https://graph.microsoft.com/v1.0/me/memberOf";
             MockHttpProvider mockHttpProvider = new MockHttpProvider();
 
@@ -80,6 +82,40 @@ namespace MGWDevGraphSDK.UT.Tests.Graph
             var response = client.Me.MemberOf.Request().GetAsync().Result;
 
             Assert.AreEqual("d17a5f86-57f4-48f8-87a0-79761dc8e706", response.FirstOrDefault().Id);
+            Assert.AreEqual("Fashion Events", response.FirstOrDefault().AdditionalData["displayName"]);
+        }
+        [TestMethod]
+        public void MeEndpoint_GetMeInfo_MemberOf_Group()
+        {
+            var sth = JsonConvert.DeserializeObject("{\"@odata.type\": \"#microsoft.graph.group\"}");
+            string requestUrl = "https://graph.microsoft.com/v1.0/me/memberOf";
+            MockHttpProvider mockHttpProvider = new MockHttpProvider();
+
+            mockHttpProvider.Responses.Add("GET:" + requestUrl, new
+            {
+                value = new List<object>()
+                {
+                    new Group(){
+                        Id =  "d17a5f86-57f4-48f8-87a0-79761dc8e706",
+                        CreatedDateTime = DateTime.Parse("2017-07-31T17:36:25Z"),
+                        Description= "Fashion events Channel ... everything you need to know all in one place. Announcements, Events, Information, and News.",
+                        DisplayName= "Fashion Events",
+                        GroupTypes= new string[] {"Unified" },
+                        Mail= "FashionEvents@M365x214355.onmicrosoft.com",
+                        MailEnabled= true,
+                            MailNickname= "FashionEvents",
+                            RenewedDateTime= DateTime.Parse("2017-07-31T17:36:25Z"),
+                            SecurityEnabled= true,
+                            SecurityIdentifier= "S-1-12-1-3514457990-1224234996-1987682439-115853341",
+                            Visibility= "Public"
+                    }
+                }
+            });
+            GraphServiceClient client = new GraphServiceClient(new MockAuthenticationHelper(), mockHttpProvider);
+            var response = client.Me.MemberOf.Request().GetAsync().Result;
+
+            Assert.AreEqual("d17a5f86-57f4-48f8-87a0-79761dc8e706", response.FirstOrDefault().Id);
+            Assert.AreEqual("Fashion Events", response.FirstOrDefault().AdditionalData["displayName"]);
         }
     }
 }

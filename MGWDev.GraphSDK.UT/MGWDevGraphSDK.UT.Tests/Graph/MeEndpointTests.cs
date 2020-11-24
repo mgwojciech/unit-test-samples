4746d8c1ad0bb31105443f2cc83111d234a0f485
Marcin Wojciechowski <marcin@valointranet.com>
2020-11-24 14:10:18 +0100

    Added member of example

--- MGWDev.GraphSDK.UT/MGWDevGraphSDK.UT.Tests/Graph/MeEndpointTests.cs
+++ MGWDev.GraphSDK.UT/MGWDevGraphSDK.UT.Tests/Graph/MeEndpointTests.cs
@@ -33,7 +33,7 @@ namespace MGWDevGraphSDK.UT.Tests.Graph
             MockHttpProvider mockHttpProvider = new MockHttpProvider();
             mockHttpProvider.OnRequestExecuting += delegate (object sender, MockRequestExecutingEventArgs eventArgs)
             {
-                if(eventArgs.RequestMessage.RequestUri.ToString() == requestUrl)
+                if (eventArgs.RequestMessage.RequestUri.ToString() == requestUrl)
                 {
                     eventArgs.Result = new User()
                     {
@@ -45,6 +45,41 @@ namespace MGWDevGraphSDK.UT.Tests.Graph
             User response = client.Me.Request().GetAsync().Result;
 
             Assert.AreEqual("Test User", response.DisplayName);
+        }
+        [TestMethod]
+        public void MeEndpoint_GetMeInfo_MemberOf()
+        {
+            string requestUrl = "https://graph.microsoft.com/v1.0/me/memberOf";
+            MockHttpProvider mockHttpProvider = new MockHttpProvider();
+
+            mockHttpProvider.Responses.Add("GET:" + requestUrl, new
+            {
+                value = new List<object>()
+                {
+                    new {
+                        id =  "d17a5f86-57f4-48f8-87a0-79761dc8e706",
+                        createdDateTime = "2017-07-31T17:36:25Z",
+                        description= "Fashion events Channel ... everything you need to know all in one place. Announcements, Events, Information, and News.",
+                        displayName= "Fashion Events",
+                        groupTypes= new string[] {"Unified" },
+                            mail= "FashionEvents@M365x214355.onmicrosoft.com",
+                            mailEnabled= true,
+                            mailNickname= "FashionEvents",
+                            proxyAddresses= new string[]{
+                            "SMTP:FashionEvents@M365x214355.onmicrosoft.com",
+                            "SPO:SPO_c1e5444e-12d8-43d3-96b1-f2f66559ef58@SPO_dcd219dd-bc68-4b9b-bf0b-4a33a796be35"
+                        },
+                            renewedDateTime= "2017-07-31T17:36:25Z",
+                            securityEnabled= true,
+                            securityIdentifier= "S-1-12-1-3514457990-1224234996-1987682439-115853341",
+                            visibility= "Public"
+                    }
+                }
+            });
+            GraphServiceClient client = new GraphServiceClient(new MockAuthenticationHelper(), mockHttpProvider);
+            var response = client.Me.MemberOf.Request().GetAsync().Result;
+
+            Assert.AreEqual("Test User", response.FirstOrDefault());
         }
     }
 }

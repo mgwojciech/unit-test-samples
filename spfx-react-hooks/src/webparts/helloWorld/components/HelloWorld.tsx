c2140eeb86c542a4924f0ed7688973a4cabcc755
Marcin Wojciechowski <marcin@valointranet.com>
2021-03-09 19:08:41 +0100

    Initial commit (React hooks)

---
+++ spfx-react-hooks/src/webparts/helloWorld/components/HelloWorld.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import styles from './HelloWorld.module.scss';
+import { SPHttpClient } from "@microsoft/sp-http";
+import { IHelloWorldProps } from './IHelloWorldProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import {PrimaryButton, Spinner } from 'office-ui-fabric-react';
+
+export default function HelloWorld(props: IHelloWorldProps){
+  
+    const [webProperties, setWebProperties] = React.useState();
+    const [welcomePage, setWelcomePage] = React.useState();
+    
+    let loadInitialData = async ()=>{
+      let response = await props.spContext.spHttpClient.get(props.spContext.pageContext.web.absoluteUrl+ "/_api/web/allProperties", SPHttpClient.configurations.v1);
+      let webProps = await response.json();
+
+      setWebProperties(webProps);
+    }
+
+    React.useEffect(()=>{
+      loadInitialData();
+    },[]);
+
+    let loadWelcomePage = async ()=>{
+      let response = await props.spContext.spHttpClient.get(props.spContext.pageContext.web.absoluteUrl+ "/_api/web?$select=WelcomePage", SPHttpClient.configurations.v1);
+      let web = await response.json();
+
+      setWelcomePage(web.WelcomePage);
+    }
+
+    return (
+      <div className={ styles.helloWorld }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>Welcome to SharePoint!</span>
+              {webProperties ? <div data-testid="web-property-div" >{webProperties.ThemePrimary}</div> : <Spinner data-testid="loading-spinner" />}
+            </div>
+          </div>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>Welcome page is</span>
+              {welcomePage ? <div data-testid="welcome-page-div" >{welcomePage}</div> : <PrimaryButton 
+              data-testid="load-welcome-page-btn"
+              onClick={(event)=>loadWelcomePage()}
+              text="Load welcome page" />}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
---
+++ spfx-react-hooks/tests/HelloWorld.test.tsx
@@ -0,0 +1,90 @@
+/// <reference types="jest" />
+import * as React from 'react';
+import { assert } from 'chai';
+import HelloWorld from "../src/webparts/helloWorld/components/HelloWorld";
+import { render, fireEvent, getByTestId} from "@testing-library/react";
+import { act } from 'react-dom/test-utils';
+
+
+jest.mock("@microsoft/sp-http", () => {
+    return {
+      SPHttpClient: {
+        configurations: {
+          v1: 1
+        }
+      },
+      HttpClient: {
+        configurations: {
+          v1: 1
+        }
+      }
+    }
+  });
+describe("<HelloWorld />", ()=>{
+    test("should render spinner", async ()=>{
+        let mockedSPContext = {
+            spHttpClient:{
+                get: ()=>Promise.resolve({
+                    json:()=>Promise.resolve({ThemePrimary: "MockTheme"})
+                })
+            },
+            pageContext:{
+                web:{
+                    absoluteUrl: "https://test.sharepoint.com/sites/tea-point"
+                }
+            }
+        }
+        let helloWorldComponent = render(<HelloWorld spContext={mockedSPContext as any}/>);
+        let spinner = await helloWorldComponent.getByTestId("loading-spinner");
+        assert.isOk(spinner);
+    });
+    test("should render loaded data", async ()=>{
+        let helloWorldComponent;
+            act(()=>{
+                let mockedSPContext = {
+                    spHttpClient:{
+                        get: ()=>Promise.resolve({
+                            json:()=>Promise.resolve({ThemePrimary: "MockTheme"})
+                        })
+                    },
+                    pageContext:{
+                        web:{
+                            absoluteUrl: "https://test.sharepoint.com/sites/tea-point"
+                        }
+                    }
+                }
+                helloWorldComponent = render(<HelloWorld spContext={mockedSPContext as any}/>);
+            });
+        await sleep(20);
+        let propertyDiv = await helloWorldComponent.getByTestId("web-property-div");
+        assert.equal(propertyDiv.textContent,"MockTheme");
+    });
+    test("should load welcome page", async ()=>{
+        let helloWorldComponent;
+            act(()=>{
+                let mockedSPContext = {
+                    spHttpClient:{
+                        get: ()=>Promise.resolve({
+                            json:()=>Promise.resolve({ThemePrimary: "MockTheme", WelcomePage: "Test.aspx"})
+                        })
+                    },
+                    pageContext:{
+                        web:{
+                            absoluteUrl: "https://test.sharepoint.com/sites/tea-point"
+                        }
+                    }
+                }
+                helloWorldComponent = render(<HelloWorld spContext={mockedSPContext as any}/>);
+            });
+        await sleep(20);
+        let loadWelcomePageBtn = await helloWorldComponent.getByTestId("load-welcome-page-btn");
+        fireEvent.click(loadWelcomePageBtn);
+        await sleep(20);
+        let propertyDiv = await helloWorldComponent.getByTestId("welcome-page-div");
+        assert.equal(propertyDiv.textContent,"Test.aspx");
+    });
+})
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
